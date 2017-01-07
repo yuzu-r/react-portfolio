6ed2044deb5208cc64a4799e57034fab4088dba2
Tracy Kaneshiro <tkaneshiro@gmail.com>
2017-01-07 18:54:07 +0000

    make keys unique

--- app/components/Header.js
+++ app/components/Header.js
@@ -14,8 +14,8 @@ var Header = React.createClass({
         <Navbar.Collapse>
           <Nav pullRight>
             <NavItem eventKey={1} href="#" className='btn  speshul'>ABOUT</NavItem>
-            <NavItem eventKey={1} href="#" className='btn btn-default speshul'>PROJECTS</NavItem>
-            <NavItem eventKey={1} href="#" className='btn btn-default speshul'>CONTACT</NavItem>
+            <NavItem eventKey={2} href="#" className='btn btn-default speshul'>PROJECTS</NavItem>
+            <NavItem eventKey={3} href="#" className='btn btn-default speshul'>CONTACT</NavItem>
           </Nav>
         </Navbar.Collapse >
       </Navbar>
