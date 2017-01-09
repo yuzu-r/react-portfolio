46e87f8193f5e69195e5a39bbcc9da5dd6805d35
Tracy Kaneshiro <tkaneshiro@gmail.com>
2017-01-09 18:18:29 +0000

    style anchors

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,5 +1,4 @@
 var React = require('react');
-var Header = require('./Header');
 var SimpleHeader = require('./SimpleHeader');
 var AboutSection = require('./AboutSection');
 var ProjectSection = require('./ProjectSection');
