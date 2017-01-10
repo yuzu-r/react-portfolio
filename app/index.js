4de77d1d4a393ea8531857734518f2e83eae945a
Tracy Kaneshiro <tkaneshiro@gmail.com>
2017-01-10 16:46:23 +0000

    add modernizr library

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Main = require('./components/Main');
+var Modernizr = require('./utils/modernizr-custom.js');
 
 ReactDOM.render(
   <Main />,
