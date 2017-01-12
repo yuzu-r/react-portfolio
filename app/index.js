8bb27aea95bf00d67ed842705f0b96520e521df0
Tracy Kaneshiro <tkaneshiro@gmail.com>
2017-01-12 18:31:34 +0000

    findIndex poly

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Main = require('./components/Main');
 var Modernizr = require('./utils/modernizr-custom.js');
+var IEPoly = require('./utils/ie_poly.js');
 
 ReactDOM.render(
   <Main />,
